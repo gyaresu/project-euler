4aaadbd09471a955e0f2ab973a72ade68b03933f
gyaresu <me@gareth.com.au>
2013-05-04 09:37:10 +0300

    on the python trail

--- euler03-largest-prime-factor.js
+++ euler03-largest-prime-factor.js
@@ -2,7 +2,8 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143
 
-var x = 600851475143;
+//var x = 600851475143;
+var x = 121;
 
 function isLargest(y) {
     for (var i=2; i<=y; i++) {
