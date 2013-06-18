64bc4ca66f080511377981c88f31b31a11cda0dd
gyaresu <me@gareth.com.au>
2013-06-18 09:31:53 +0100

    03 and 04 in py

---
+++ euler04.py
@@ -0,0 +1,5 @@
+// https://projecteuler.net/problem=4
+//
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 3-digit numbers is 9009 = 91 99.
+//
+// Find the largest palindrome made from the product of two 3-digit numbers.
