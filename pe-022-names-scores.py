96d352041278ecf5a6a3bdc9cc451e8799edb3f8
gyaresu <me@gareth.com.au>
2014-02-19 12:28:32 +0100

    finished 22

--- pe-022-names-scores.py
+++ pe-022-names-scores.py
@@ -2,8 +2,29 @@ import shlex as sh
 
 a = open("names.txt")
 b = a.read()
+print "Your file %r loaded." % "names.txt"
+a.close()
+
 c = sh.split(b)
 d = c[0].split(",")
 d.sort()
+e = []
+alpha = "abcdefghijklmnopqrstuvwxyz"
+count = 1
+total = 0
 
+for item in d:
+  e.append(item.lower())
 
+for item in e:
+  temp = 0
+  for each in item:
+    #print each
+    #print alpha.index(each) + 1
+    temp += alpha.index(each) + 1
+
+  total += count * temp
+  count += 1
+  #print temp
+  #print total
+print total
